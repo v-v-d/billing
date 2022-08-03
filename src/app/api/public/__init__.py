ec9f41d239e3e24c366439bbe0628e7cb0863b8c
Viktor.Vasilev <Viktor.Vasilev@x5.ru>
2022-08-04 02:23:32 +0300

    #8-base-service-code all tests passed

--- src/app/api/public/__init__.py
+++ src/app/api/public/__init__.py
@@ -4,7 +4,7 @@ from app.api.dependencies.auth import decode_jwt
 from app.api.public import v1
 from app.api.schemas import ErrorSchema
 
-admin_api = APIRouter(
+public_api = APIRouter(
     responses={
         status.HTTP_401_UNAUTHORIZED: {"model": ErrorSchema},
         status.HTTP_403_FORBIDDEN: {"model": ErrorSchema},
@@ -12,5 +12,5 @@ admin_api = APIRouter(
     dependencies=[Depends(decode_jwt)],
 )
 
-admin_api.include_router(v1.receipts.router, prefix="/v1/receipts")
-admin_api.include_router(v1.transactions.router, prefix="/v1/transactions")
+public_api.include_router(v1.receipts.router, prefix="/v1/receipts")
+public_api.include_router(v1.transactions.router, prefix="/v1/transactions")
