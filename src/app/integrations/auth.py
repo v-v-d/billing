38dcab7de533666b20e61297bc9f966d535c59ab
pert <petr777@yahoo.com>
2022-08-16 20:59:36 +0300

    update UserSchemar

--- src/app/integrations/auth.py
+++ src/app/integrations/auth.py
@@ -4,14 +4,16 @@ from pydantic import AnyHttpUrl, ValidationError, BaseModel, UUID4
 from app.integrations.base import AbstractHttpClient
 from app.settings import settings
 from app.transports import AbstractHttpTransport, AiohttpTransport
-
+from typing import Optional
 
 class AuthHttpClientError(Exception):
     pass
 
 
 class UserSchema(BaseModel):
-    pass
+    id: UUID4
+    email: Optional[str]
+    phone: Optional[str]
 
 
 class AuthHttpClient(AbstractHttpClient):
@@ -22,12 +24,10 @@ class AuthHttpClient(AbstractHttpClient):
         self.http_transport: AbstractHttpTransport = http_transport
 
     async def get_user_details(self, user_id: UUID4) -> UserSchema:
-        url = furl(self.base_url).add(path="/api/internal/v1/users").add(path=user_id)
-
+        url = furl(self.base_url).add(path="/api/internal/v1/users").add(path=user_id).add(path='info')
         response = await self.request(
             method="GET", url=url.url, timeout=settings.AUTH_INTEGRATION.TIMEOUT_SEC
         )
-
         try:
             return UserSchema(**response)
         except ValidationError as err:
