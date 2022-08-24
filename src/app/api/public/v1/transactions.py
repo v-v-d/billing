dcc75b9cdd874bd2434b937068e2d58f502942d6
Евгений Балякин <balyakin@algont.ru>
2022-08-24 17:12:53 +0300

    first version of /transactions/{transaction_id} endpoint

--- src/app/api/public/v1/transactions.py
+++ src/app/api/public/v1/transactions.py
@@ -1,10 +1,12 @@
 import sqlalchemy as sa
-from fastapi import APIRouter, Depends
+from http import HTTPStatus
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi_pagination import LimitOffsetPage
 from fastapi_pagination.ext.async_sqlalchemy import paginate
 from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import UUID4
 
-from app.models import Transaction
+from app.models import Transaction, ObjectDoesNotExistError
 from app.api.dependencies.database import get_db
 from app.api.admin.v1.schemas import TransactionOutput
 from app.security import TokenData, decode_jwt_token
@@ -25,3 +27,22 @@ async def get_users_transactions(
     return paginate(
         db_session, sa.select(Transaction).where(Transaction.user_id == user_id)
     )
+
+
+@router.get(
+    "/{transaction_id}",
+    response_model=TransactionOutput,
+    description="Retrieve transaction",
+)
+async def get_template_by_id(
+    transaction_id: UUID4,
+    db_session: AsyncSession = Depends(get_db),
+):
+    try:
+        template = await Transaction.get(db_session, id=transaction_id)
+    except ObjectDoesNotExistError:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Transaction not found"
+        )
+
+    return TransactionOutput.from_orm(template)
