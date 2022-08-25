596f1dd62e338191c4c6041d1a9dfbf82abee9c5
Евгений Балякин <balyakin@algont.ru>
2022-08-25 14:37:30 +0300

    added transactions tests

--- src/tests/functional/src/api/public/v1/test_transactions.py
+++ src/tests/functional/src/api/public/v1/test_transactions.py
@@ -11,6 +11,7 @@ from tests.functional.utils import fake
 
 pytestmark = pytest.mark.asyncio
 
+
 @pytest.fixture
 async def user_id() -> str:
     return fake.cryptographic.uuid()
@@ -43,6 +44,12 @@ def headers(valid_jwt_token) -> dict[str, Any]:
 async def db_data(
     db_session, transaction_id, transaction_ext_id, receipt_id, valid_jwt_payload
 ) -> None:
+
+    stmt2 = sa.delete(Receipt)
+    await db_session.execute(stmt2)
+    stmt1 = sa.delete(Transaction)
+    await db_session.execute(stmt1)
+
     stmt1 = sa.insert(Transaction).values(
         id=transaction_id,
         ext_id=transaction_ext_id,
@@ -62,33 +69,33 @@ async def db_data(
     await db_session.execute(stmt2)
 
 
-
 async def test_user_transactions(
     client,
     valid_jwt_payload,
     valid_jwt_token,
     headers,
-    db_session,
     db_data,
     transaction_id,
+    transaction_ext_id,
 ) -> None:
 
     response = await client.get(
         path=app.url_path_for(name="get_users_transactions"),
         headers=headers,
     )
-
     assert response.status_code == 200, response.text
+    assert len(response.json()["items"]) == 1
+    assert response.json()["items"][0]["id"] == transaction_id
+    assert response.json()["items"][0]["ext_id"] == transaction_ext_id
 
 
 async def test_admin_transactions(
     client,
     valid_jwt_payload,
-    valid_jwt_token,
     headers,
-    db_session,
     db_data,
     transaction_id,
+    transaction_ext_id,
 ) -> None:
 
     response = await client.get(
@@ -96,21 +103,27 @@ async def test_admin_transactions(
     )
 
     assert response.status_code == 200, response.text
-
+    assert len(response.json()["items"]) == 1
+    assert response.json()["items"][0]["id"] == transaction_id
+    assert response.json()["items"][0]["ext_id"] == transaction_ext_id
 
 
 async def test_transaction_by_id(
     client,
     valid_jwt_payload,
-    valid_jwt_token,
     headers,
-    db_session,
     db_data,
     transaction_id,
+    transaction_ext_id,
+    receipt_id,
 ) -> None:
     response = await client.get(
-        path=app.url_path_for(name="get_transaction_by_id", transaction_id=transaction_id),
+        path=app.url_path_for(
+            name="get_transaction_by_id", transaction_id=transaction_id
+        ),
         headers=headers,
     )
-
     assert response.status_code == 200, response.text
+    assert response.json()["id"] == transaction_id
+    assert response.json()["ext_id"] == transaction_ext_id
+    assert response.json()["receipts"][0]["id"] == receipt_id
