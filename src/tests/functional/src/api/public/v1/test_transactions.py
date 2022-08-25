07013f6a7373d732531faece7c2f399211c33a20
Евгений Балякин <balyakin@algont.ru>
2022-08-25 17:58:44 +0300

    added transactions tests

--- src/tests/functional/src/api/public/v1/test_transactions.py
+++ src/tests/functional/src/api/public/v1/test_transactions.py
@@ -77,35 +77,6 @@ async def db_data(
     await db_session.execute(stmt2)
 
 
-@pytest.fixture
-async def db_data_admin(
-    db_session, transaction_id, transaction_ext_id, receipt_id, valid_admin_payload
-) -> None:
-
-    stmt2 = sa.delete(Receipt)
-    await db_session.execute(stmt2)
-    stmt1 = sa.delete(Transaction)
-    await db_session.execute(stmt1)
-
-    stmt1 = sa.insert(Transaction).values(
-        id=transaction_id,
-        ext_id=transaction_ext_id,
-        user_id=valid_admin_payload["user_id"],
-        amount=400,
-        type=Transaction.TypeEnum.PAYMENT,
-        payment_type=Transaction.PaymentType.CARD,
-    )
-
-    await db_session.execute(stmt1)
-
-    stmt2 = sa.insert(Receipt).values(
-        id=receipt_id,
-        ext_id=transaction_ext_id,
-        transaction_id=transaction_id,
-    )
-    await db_session.execute(stmt2)
-
-
 async def test_user_transactions(
     client, headers, db_data, transaction_id, transaction_ext_id, valid_jwt_payload
 ) -> None:
@@ -119,7 +90,7 @@ async def test_user_transactions(
     assert response.json()["items"][0]["id"] == transaction_id
     assert response.json()["items"][0]["ext_id"] == transaction_ext_id
     assert response.json()["items"][0]["amount"] == 400
-    assert response.json()["items"][0]["user_id"] == valid_jwt_payload['user_id']
+    assert response.json()["items"][0]["user_id"] == valid_jwt_payload["user_id"]
 
 
 async def test_admin_transactions(
