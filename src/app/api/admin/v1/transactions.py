4ec394e514114714248f22e0df05e2f990b871ae
Евгений Балякин <balyakin@algont.ru>
2022-08-26 09:00:27 +0300

    added transactions enpoints

--- src/app/api/admin/v1/transactions.py
+++ src/app/api/admin/v1/transactions.py
@@ -1,3 +1,26 @@
-from fastapi import APIRouter
+import sqlalchemy as sa
+from fastapi import APIRouter, Depends
+from fastapi_pagination import LimitOffsetPage
+from fastapi_pagination.ext.async_sqlalchemy import paginate
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from app.models import Transaction
+from app.api.dependencies.database import get_db
+from app.api.admin.v1.schemas import TransactionOutput
 
 router = APIRouter()
+
+
+@router.get(
+    "/",
+    response_model=LimitOffsetPage[TransactionOutput],
+    description="Retrieve transactions",
+)
+async def get_transactions(db_session: AsyncSession = Depends(get_db)):
+    return await paginate(
+        db_session,
+        sa.select(Transaction)
+        .options(selectinload(Transaction.receipt))
+        .options(selectinload(Transaction.user_film)),
+    )
